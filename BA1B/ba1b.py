9112485e41602da26e0ae21805d1c44b329ab710
Marina <61973540+ml-gitgud@users.noreply.github.com>
2022-10-23 15:01:58 +0300

    Cleanup

--- BA1B/ba1b.py
+++ BA1B/ba1b.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
 
 
-def list_kmers(dna, k):
-    '''
-    Returns a list of all k-mers in a string
-    '''
-    return [dna[i:i + k] for i in range(len(dna) - k + 1)]
-
-
 def frequent_words(dna, k):
-    '''
-    Returns a list of most occuring k-mers
-    '''
+    """
+    Returns a list of most occuring k-mers in the dna
+    """
     frequencies = {}
     max_occurrence = 0
-    for i in range(0, len(dna) - k + 1):
-        kmer = dna[i:i + k]
-        if kmer in frequencies.keys():
-            frequencies[kmer] += 1
-        else:
-            frequencies[kmer] = 1
+    for kmer in (dna[i : i + k] for i in range(len(dna) - k + 1)):
+        frequencies[kmer] = frequencies.get(kmer, 0) + 1
         if frequencies[kmer] > max_occurrence:
             max_occurrence = frequencies[kmer]
     kmers = []
@@ -29,10 +18,10 @@ def frequent_words(dna, k):
     return kmers
 
 
-if __name__ == '__main__':
-    with open('./rosalind_ba1b.txt', 'r') as dataset:
-        dna = dataset.readline().strip()
+if __name__ == "__main__":
+    with open("./rosalind_ba1b.txt", "r") as dataset:
+        dna = dataset.readline().rstrip()
         k = int(dataset.readline())
     solution = frequent_words(dna, k)
-    with open('./solution_ba1b.txt', 'w') as file:
-        file.write('\n'.join(solution))
+    with open("./solution_ba1b.txt", "w") as file:
+        file.write("\n".join(solution))
