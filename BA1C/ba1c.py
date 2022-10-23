78e397c642fcf7fd9f14b249e3435fdab6828ced
Marina <61973540+ml-gitgud@users.noreply.github.com>
2022-10-23 19:15:58 +0300

    Solution to BA1C problem

---
+++ BA1C/ba1c.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def reverse_complement(pattern):
+    """
+    Returns a reverse complement of a dna string pattern.
+    """
+    dict = pattern.maketrans("ATGC", "TACG")
+    return pattern.translate(dict)[::-1]
+
+
+if __name__ == "__main__":
+    with open("./rosalind_ba1c.txt", "r") as dataset:
+        pattern = dataset.readline().rstrip()
+    solution = reverse_complement(pattern)
+    with open("./solution_ba1c.txt", "w") as file:
+        file.write(solution)
