23e713d29254f2980b421174de906c3889e7847a
Marina <61973540+ml-gitgud@users.noreply.github.com>
2022-10-23 15:53:30 +0300

    Cleanup

--- BA1B/ba1b.py
+++ BA1B/ba1b.py
@@ -20,7 +20,7 @@ def frequent_words(dna, k):
 
 if __name__ == "__main__":
     with open("./rosalind_ba1b.txt", "r") as dataset:
-        dna = dataset.readline().rstrip()
+        dna = dataset.readline().strip()
         k = int(dataset.readline())
     solution = frequent_words(dna, k)
     with open("./solution_ba1b.txt", "w") as file:
